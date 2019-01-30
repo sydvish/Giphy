a6a2520e5f3ff39ba5a36b4857e02bf3f303321b
sydney Volk <sydneyvolk@sydneys-MacBook-Pro.local>
2019-01-30 14:32:36 -0700

    final version

--- javascript/app.js
+++ javascript/app.js
@@ -34,7 +34,7 @@ function renderButtons() {
 
     for (var i = 0; i < topics.length; i++) {
         var a = $("<button>");
-        a.addClass("topics");
+        a.addClass("topics btn btn-sage");
         a.attr("data-name", topics[i]);
         a.text(topics[i]);
         $("#buttons-view").append(a);
@@ -44,32 +44,68 @@ function renderButtons() {
 $("#add-topic").on("click", function (event) {
     event.preventDefault();
     var topic = $("#topic-input").val().trim();
-    // console.log(topic);
     topics.push(topic);
-    // console.log(topic);
     renderButtons();
 });
 
-// I don't think lines 56 & 57 are working properly
+$(document).on("click", ".gif",function() {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
+
 
 $('#buttons-view').on('click', ".topics", function () {
-    // console.log("button clicked!");
     var searchTopic = $(this).attr("data-name");
     console.log(searchTopic);
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTopic + "&api_key=dc6zaTOxFJmzC&limit=10";
-    console.log(queryURL);
-    
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTopic + "&api_key=dc6zaTOxFJmzC&limit=10&rating=pg";
+   
 
     $.ajax({
         url: queryURL,
         method: 'GET'
     }).then(function (response) {
+        $("#gifArea").empty();
         console.log(response);
+        // console.log(response.data.length);
+        for (var i = 0; i < response.data.length; i++) {
+            var gif = response.data[i];
+            // create a gif bucket
+            var $div = $("<div class='col-12 col-md-6'>");
+            // make a p tag to store our rating
+            var $rating = $("<p>").text("Rating: " + gif.rating);
+            // create an image tag
+            var $img = $("<img>");
+            // set img src to still img url
+            // and an attribute that pulls the original static image url from the ajax 
+            $img.attr("src", gif.images.original_still.url);
+            $img.attr("data-still", gif.images.original_still.url);
+            $img.attr("data-animate", gif.images.original.url);
+            $img.attr("data-state", "still");
+            $img.attr("class", "gif img-fluid");
+
+            // Designate the div area for the still image and the rating
+            // append our rating p tag and img tag
+            $div.append($rating, $img);
+            // append the image and rating info to the specific '#gifArea'
+            $("#gifArea").append($div);
+
+        };
+
     });
 });
 
 
-// Dynamically turn them into buttons on my webpage
+
 
 // When the user clicks a button, 10 static gifs should appear from API with rating displayed
 
